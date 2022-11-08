a5e2f71ca84b310be4493d6465e50405758040e1
a-stanisz <hello@adamstaniszewski.com>
2022-11-08 12:55:15 +0100

    Add error middleware for ValidationError

--- services/movie-service/src/server.js
+++ services/movie-service/src/server.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const dbConnect = require('./database/connect');
-const resetLimitCounters = require('./workers/resetCounter');
+const express = require("express");
+const dbConnect = require("./database/connect");
+const resetLimitCounters = require("./workers/resetCounter");
+
+const { ValidationError } = require("express-json-validator-middleware");
 
 dbConnect();
 
 const PORT = process.env.MOVIE_SERVICE_PORT || 8080;
 
-const diaryRoutes = require('./routes/diary');
+const diaryRoutes = require("./routes/diary");
 const app = express();
 
 app.use(express.json());
+
+app.use((error, _, res, __) => {
+  try {
+    if (res.headersSent) {
+      return next(error);
+    }
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      res.status(400).json({ errors: error.validationErrors });
+    }
+  }
+  next(error);
+});
 
 app.use((error, _, res, __) => {
   console.error(
@@ -19,10 +34,10 @@ app.use((error, _, res, __) => {
   return res.status(500).json({ error: "internal server error" });
 });
 
-app.use('/', diaryRoutes);
+app.use("/", diaryRoutes);
 
-app.get('/', (req, res, next) => {
-  res.send('You are accessing the root route of the movie-service. Hello!')
+app.get("/", (req, res, next) => {
+  res.send("You are accessing the root route of the movie-service. Hello!");
 });
 
 app.listen(PORT, () => {
