ef663397070e9e754422aa622ac8621d7b8b9481
a-stanisz <hello@adamstaniszewski.com>
2022-11-08 13:03:41 +0100

    Add request's body validation

--- services/movie-service/src/middleware/getTitleToSearch.js
+++ services/movie-service/src/middleware/getTitleToSearch.js
@@ -1,15 +1,20 @@
+const { Validator } = require("express-json-validator-middleware");
+const validator = new Validator();
+const movieSchema = require("../domain/movie.schema.json");
+validator.ajv.addSchema(movieSchema, "movieSchema");
+
 module.exports = async (req, res, next) => {
   try {
+    const body = req.body;
     if (!req.body) {
       return res.status(400).json({ error: "invalid payload" });
     }
-    const { title } = req.body;
-    if (!title) {
-      return res.status(400).json({ error: "invalid payload" });
-    }
+    const title = body.title;
+    validator.validate({ title: movieSchema });
     req.searchStr = title;
   } catch (error) {
-    next();
+    console.error(error);
+    next(error);
   }
   next();
 };
