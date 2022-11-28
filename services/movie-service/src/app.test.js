0411b5ca12e53a8d8181ccab7c2b7dd160cf3f70
a-stanisz <hello@adamstaniszewski.com>
2022-11-28 15:34:03 +0100

    Try to mock auth-service

--- services/movie-service/src/app.test.js
+++ services/movie-service/src/app.test.js
@@ -1,53 +1,74 @@
 const request = require("supertest");
-const axios = require("axios");
+const jwt = require("jsonwebtoken");
+// const axios = require("axios");
 
 const app = require("./app");
+// const auth = require("../../auth-service/src/app");
 
-jest.mock("axios");
+const { JWT_SECRET, TEST_TOKEN } = process.env;
 
-const testUsersCreds = [
-  {
-    username: "basic-thomas",
-    password: "sR-_pcoow-27-6PAwCD8",
-  },
-  {
-    username: "premium-jim",
-    password: "GBLtTyq3E_UNjFnpo9m6",
-  },
-];
+jest.mock('jsonwebtoken');
 
-const getUserToken = async (creds) => {
-  try {
-    const authURL = `http://auth-service:3001/auth`;
-    let success;
-    const res = await axios.post(authURL, {
-      username: creds.username,
-      password: creds.password,
-    });
-    res.data.Response === "True" ? (success = true) : (success = false);
-    if (success) {
-      return Promise.resolve(res.data.token);
-    }
-    throw new Error(`auth-service error: ${data.Error}`);
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const testUsersCreds = [
+//   {
+//     username: "basic-thomas",
+//     password: "sR-_pcoow-27-6PAwCD8",
+//   },
+//   {
+//     username: "premium-jim",
+//     password: "GBLtTyq3E_UNjFnpo9m6",
+//   },
+// ];
 
-async function getTokens() {
-  try {
-    const tokens = testUsersCreds.map(async (creds) => {
-      await getUserToken(creds);
-    });
-    return tokens;
-  } catch (error) {
-    console.error(error);
-  }
-}
+// const getUserToken = async (creds) => {
+//   try {
+//     const authURL = `http://auth-service:3001/auth`;
+//     let success;
+//     const res = await axios.post(authURL, {
+//       username: creds.username,
+//       password: creds.password,
+//     });
+//     res.data.Response === "True" ? (success = true) : (success = false);
+//     if (success) {
+//       return res.data.token;
+//     }
+//     // throw new Error(`auth-service error: ${res.data.Error}`);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-(async () => {
-  console.log(await getTokens());
-})();
+// async function getTokens() {
+//   try {
+//     const tokens = await Promise.all(
+//       testUsersCreds.map(async (creds) => {
+//         return await Promise.resolve(getUserToken(creds));
+//       })
+//     );
+//     // console.log(tokens);
+//     return tokens;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// (async () => {
+//   let result = await getTokens();
+//   console.log(result);
+// })();
+
+// const token = await getTokens()[0];
+// console.log(token);
+
+// let token = "";
+
+// beforeAll(async () => {
+//   const response = await supertest(auth).post("/auth", {
+//     username: "premium-jim",
+//     password: "GBLtTyq3E_UNjFnpo9m6",
+//   });
+//   token = response.body.token;
+// });
 
 describe("POST /movies", () => {
   describe("for any user role", () => {
@@ -56,12 +77,13 @@ describe("POST /movies", () => {
       // should send structured-json response indicating successs
       test("should respond with a 200 status code", async () => {
         try {
+          // const token = await getTokens()[0];
           const response = await request(app)
             .post("/movies")
-            .set("Authorization", `Bearer ${await getTokens()}`)
+            .set("Authorization", `Bearer ${TEST_TOKEN}`)
             .set("Content-Type", "application/json")
             .send({ title: "The Last Duel" });
-          await expect(response.statusCode).resolves.toBe(200);
+          expect(response.statusCode).toBe(200);
         } catch (error) {
           console.error(error);
         }
