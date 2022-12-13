de2a5fe8ca611a973f7b2bb15bd9fac0fd18cb5e
a-stanisz <hello@adamstaniszewski.com>
2022-12-13 15:59:00 +0100

    Update user-repository

--- services/movie-service/src/database/repositories/user-repository.js
+++ services/movie-service/src/database/repositories/user-repository.js
@@ -1,4 +1,21 @@
 const User = require("../models/user");
 
-module.exports = async function addUser(userDetails) {}
-module.exports = async function getuser(userDetails) {}
+module.exports = async function updateUser(userData) {
+  try {
+    let user;
+    user = await User.findOne({ userId: userData.userId });
+    if (!user) {
+      user = new User({
+        userId: userData.userId,
+        userName: userData.name,
+        userRole: userData.role,
+        serviceUsage: { ...userData.serviceUsage },
+      });
+      await user.save();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = async function getuser(userDetails) {};
