e5f0eab5a62963d6771c5e0a3f75e97d6a2a3c78
a-stanisz <hello@adamstaniszewski.com>
2022-12-29 22:56:50 +0100

    Disable unused code

--- services/movie-service/src/api/middleware/updateUser.js
+++ services/movie-service/src/api/middleware/updateUser.js
@@ -1,28 +1,28 @@
-const User = require('../../database/models/user');
-// const basicUsersServiceUsageLimit = 5;
+// const User = require('../../database/models/user');
+// // const basicUsersServiceUsageLimit = 5;
 
-module.exports = async (req, res, next) => {
-  try {
-    let user;
-    user = await User.findOne({ userId: req.user.userId });
-    if (!user) {
-      user = new User({
-        userId: req.user.userId,
-        userName: req.user.name,
-        userRole: req.user.role,
-      });
-      // let limitation;
-      // user.userRole === 'basic' ? (limitation = true) : (limitation = false);
-      // user.serviceUsage.isLimited = limitation;
-      // if (user.serviceUsage.isLimited === true) {
-      //   user.serviceUsage.limit = basicUsersServiceUsageLimit;
-      //   user.serviceUsage.counter = 0;
-      // }
-      await user.save();
-    }
-    req.user = user;
-  } catch (err) {
-    next();
-  }
-  next();
-};
+// module.exports = async (req, res, next) => {
+//   try {
+//     let user;
+//     user = await User.findOne({ userId: req.user.userId });
+//     if (!user) {
+//       user = new User({
+//         userId: req.user.userId,
+//         userName: req.user.name,
+//         userRole: req.user.role,
+//       });
+//       // let limitation;
+//       // user.userRole === 'basic' ? (limitation = true) : (limitation = false);
+//       // user.serviceUsage.isLimited = limitation;
+//       // if (user.serviceUsage.isLimited === true) {
+//       //   user.serviceUsage.limit = basicUsersServiceUsageLimit;
+//       //   user.serviceUsage.counter = 0;
+//       // }
+//       await user.save();
+//     }
+//     req.user = user;
+//   } catch (err) {
+//     next();
+//   }
+//   next();
+// };
--- services/movie-service/src/api/middleware/validateToken.js
+++ services/movie-service/src/api/middleware/validateToken.js
@@ -1,16 +1,16 @@
-const userValidation = require('../../domain/userValidation');
-const { ValidationError } = require('../../shared/AppErrors');
+// const userValidation = require('../../domain/userValidation');
+// const { ValidationError } = require('../../shared/AppErrors');
 
-module.exports = async (req, res, next) => {
-  try {
-    const token = req.decodedToken;
-    if (!userValidation(token)) {
-      throw new ValidationError('user login data validation failed!');
-    }
-    req.user = token;
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-  next();
-};
+// module.exports = async (req, res, next) => {
+//   try {
+//     const token = req.decodedToken;
+//     if (!userValidation(token)) {
+//       throw new ValidationError('user login data validation failed!');
+//     }
+//     req.user = token;
+//   } catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+//   next();
+// };
